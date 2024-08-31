f8b06c1ef48cea08e85b8bca7f58e557ebb1156e
NotName465 <not_name00@mail.ru>
2024-08-31 11:19:45 +0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,27 @@
+// import { Telegraf, Markup, webApp} from "telegraf"
+
+function myFunc() {
+    alert('Button clicked!');
+}
+
+// Функция для вызова сканера QR-кода
+// function scanQrCode() {
+//     // Параметры для сканера QR-кода
+//     const params = {
+//         text: 'Наведите камеру на QR-код',
+//         need_phone_number: false,
+//         need_email: false,
+//         need_shipping_address: false
+//     };
+//
+//     // Вызов функции showScanQrPopup с параметрами и callback-функцией
+//     window.Telegram.WebApp.showScanQrPopup(params, (result) => {
+//         if (result.data) {
+//             // Обработка результатов сканирования
+//             console.log('QR-код сканирован:', result.data);
+//             // Здесь можно добавить логику для сравнения QR-кодов или других действий
+//         } else {
+//             console.log('Не удалось сканировать QR-код');
+//         }
+//     });
+// }
